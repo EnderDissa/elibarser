bf3b91acabb2ea4cc611fb7398e0c8e9c3890593
Denis Berman <secondissa@yandex.ru>
2025-03-24 22:40:53 +0300

    fix

--- main.py
+++ main.py
@@ -5,136 +5,166 @@ import time
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import random
 
 def extract_names_from_table(file_path):
-    # Чтение HTML в DataFrame
-    df = pd.read_html(file_path, header=0)[0]
+    df = pd.read_excel(file_path, header=3, usecols=[0, 1, 2])
+    full_names = []
 
-    # Если столбец с фамилией и именем называется, например, 'ФИО', замените его название
-    # Для примера, допустим, колонка с фамилией и именем называется 'ФИО'
     for index, row in df.iterrows():
-        full_name = row['ФИО']  # Пример: замените 'ФИО' на ваш столбец
-        name_parts = full_name.split()  # Разделяем ФИО по пробелу
-        if len(name_parts) >= 2:
-            surname = name_parts[0]  # Фамилия
-            initials = " ".join(name_parts[1:3])  # Имя и отчество
-            print(f"Фамилия: {surname}, И.О.: {initials}")
+        surname = row[0]
+        first_name = row[1]
+        patronymic = row[2]
+        initials = f"{first_name[0]}.{patronymic[0]}."
+        full_name = f"{surname} {initials}"
+        full_names.append((surname, first_name, patronymic))
 
+    return full_names
 
 options = Options()
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
     'Connection': 'keep-alive',
+    'Referer': 'https://elibrary.ru/'
 }
-options.headless = False
-SLEEP = 5
 
-def run_script(first_time_flag, author_name, reset_checkboxes=True):
+options.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+options.headless = False
+
+def calc_sleep(SLEEP=1):
+    SLEEPY = SLEEP * random.uniform(1, 3)
+    print("Sleeping for {} seconds.".format(SLEEPY))
+    return SLEEPY
+
+def run_script(first_time_flag, author_name, begin_year="2023", end_year="2025", reset_checkboxes=True):
     url = 'https://elibrary.ru/querybox.asp'
     driver.get(url)
 
     if "page_captcha.asp" in driver.current_url:
         print("Captcha detected, waiting for user to solve...")
-        time.sleep(10)
+        time.sleep(15)
         driver.refresh()
         return "CAPTCHA"
 
-    time.sleep(SLEEP)
+    time.sleep(calc_sleep())
 
-    if reset_checkboxes:
-        # Сбрасываем форму и чекбоксы перед каждым запросом
-        driver.find_element(By.NAME, 'surname').clear()  # Очистим поле автор
-        driver.find_element(By.NAME, 'surname').send_keys(author_name)  # Вводим нового автора
+    authors = driver.find_elements(By.XPATH, '//select[@name="authors"]/option')
+    if authors:
+        author = authors[0]
+        author.click()
 
-        # Сбрасываем все чекбоксы
-        driver.find_element(By.NAME, 'type_article').click()
-        driver.find_element(By.NAME, 'type_book').click()
-        driver.find_element(By.NAME, 'type_conf').click()
+    delete_buttons = driver.find_elements(By.XPATH, '//a[text()="Удалить"]')
+    if len(delete_buttons) > 1:
+        delete_button = delete_buttons[1]
+        delete_button.click()
+    time.sleep(calc_sleep())
 
     add_buttons = driver.find_elements(By.XPATH, '//a[text()="Добавить"]')
     if len(add_buttons) > 1:
         add_button = add_buttons[1]
         add_button.click()
-    else:
-        print("Не найдено второго элемента 'Добавить'.")
-        return
 
-    time.sleep(SLEEP)
-
+    time.sleep(calc_sleep())
     windows = driver.window_handles
     driver.switch_to.window(windows[1])
-
     search_input = driver.find_element(By.NAME, 'qwd')
     search_input.send_keys(author_name)
-
     search_link = driver.find_element(By.XPATH, '//a[text()="Поиск"]')
     search_link.click()
+    time.sleep(calc_sleep())
 
-    author_link = driver.find_element(By.XPATH, '/html/body/center/form/table[2]/tbody/tr[2]/td[2]/a')
-    author_link.click()
 
-    time.sleep(SLEEP)
+    authors = driver.find_elements(By.XPATH, '//td[@align="left" and @valign="middle"]/a')
 
-    driver.execute_script("location.reload(true)")  # Перезагружаем страницу, очищая кэш и сбрасывая чекбоксы
+    for i in range(len(authors)):
+        works_count = driver.find_elements(By.XPATH,
+                                           f'//a[text()="{authors[i].text.strip()}"]/../../following-sibling::tr/td/font[@color="#00008f"]')
+        if works_count:
+            num_works = int(works_count[0].text.strip())
+            if num_works > 15:
+                print(f"У {authors[i].text.strip()} больше 15 работ ({num_works}). Переход к следующему автору.")
+                if i == len(authors) - 1:
+                    return
+                continue
+        authors[i].click()
+        time.sleep(calc_sleep())
+        break
 
     driver.switch_to.window(windows[0])
+
+    begin_year_select = driver.find_element(By.NAME, "begin_year")
+    end_year_select = driver.find_element(By.NAME, "end_year")
+
+    begin_year_select.send_keys(begin_year)
+    end_year_select.send_keys(end_year)
+
+    if reset_checkboxes:
+        article_checkbox = driver.find_element(By.NAME, 'type_article')
+        book_checkbox = driver.find_element(By.NAME, 'type_book')
+        conf_checkbox = driver.find_element(By.NAME, 'type_conf')
+
+        if not article_checkbox.is_selected():
+            driver.find_element(By.NAME, 'type_article').click()
+        if not book_checkbox.is_selected():
+            driver.find_element(By.NAME, 'type_book').click()
+        if not conf_checkbox.is_selected():
+            driver.find_element(By.NAME, 'type_conf').click()
+
     search_button = driver.find_element(By.XPATH, '//a[text()="Поиск"]')
     search_button.click()
-
-    time.sleep(SLEEP)
+    time.sleep(calc_sleep())
 
     html_content = driver.page_source
-
     soup = BeautifulSoup(html_content, 'html.parser')
 
     book_links = soup.find_all('a', href=True)
-    book_urls = []
+    book_urls = {}
 
     for link in book_links:
         href = link['href']
         if '/item.asp?id=' in href:
             full_url = 'https://www.elibrary.ru' + href
-            book_urls.append(full_url)
+            title_tag = link.find('span', style="line-height:1.0;")
+            title = title_tag.text.strip() if title_tag else "Без названия"
+            book_urls[full_url] = title
+    time.sleep(calc_sleep())
 
-    print("Ссылки на книги:")
-    for url in book_urls:
-        print(url)
-
-    doi_list = []
-
-    for book_url in book_urls:
-        response = requests.get(book_url, headers=headers)
-        page_soup = BeautifulSoup(response.text, 'html.parser')
-
-        doi_tags = page_soup.find_all('a', href=True)
-        for doi_tag in doi_tags:
-            href = doi_tag['href']
-            if "doi.org" in href:
-                doi = href.split("doi.org/")[-1]
-                doi_list.append(doi)
-
-        time.sleep(SLEEP)
-
-    print("\nDOI для каждой публикации:")
-    for doi in doi_list:
-        print(doi)
-
-    with open('elibrary_querybox_results.html', 'w', encoding='utf-8') as file:
-        file.write(html_content)
-
-    print("HTML страница с результатами успешно сохранена.")
+    return book_urls
 
 first_time_flag = 1
 driver = webdriver.Firefox(options=options)
 
-while True:
-    author_name = "Конокин Д В"  # Пример: замени на имя автора, которое нужно искать
-    result = run_script(first_time_flag, author_name)
-    first_time_flag = 0
-    if result == "CAPTCHA":
-        first_time_flag = 1
-        driver.close()
-        driver = webdriver.Firefox(options=options)
-        time.sleep(10)
+names = extract_names_from_table("вход.xlsx")
+print(names)
+for name in names:
+    try:
+        results = []
+        author_name = f"{name[0]} {name[1][0]}.{name[2][0]}."
+        print(author_name)
+        result = run_script(first_time_flag, author_name, begin_year="2023", end_year="2025")
+        first_time_flag = 0
+        if result == "CAPTCHA":
+            print(f"CAPTCHA обнаружена для {author_name}, ждем, пока она будет решена...")
+            while result == "CAPTCHA":
+                result = run_script(first_time_flag, author_name, begin_year="2023", end_year="2025")
+
+        if result:
+            for book_url, title in result.items():
+                results.append((name[0], name[1], name[2], title, book_url))
+            df_results = pd.DataFrame(results, columns=["Фамилия", "Имя", "Отчество", "Название статьи", "Ссылка на статью"])
+
+            try:
+                existing_df = pd.read_excel("вывод.xlsx")
+                with pd.ExcelWriter("вывод.xlsx", engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+                    df_results.to_excel(writer, index=False, header=False, startrow=len(existing_df) + 1)
+            except FileNotFoundError:
+                df_results.to_excel("вывод.xlsx", index=False)
+        else:
+            print(f"Для {author_name} не найдено подходящих работ или есть больше 15 работ.")
+    except Exception as e:
+        print("страшная вонючая ошибка " + str(e))
+
+
+print("Результаты сохранены в файл 'вывод.xlsx'.")
